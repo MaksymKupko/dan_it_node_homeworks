470abdf31066a9ba572774c7cc407ee5901dfa27
Maksym Kupko <maksym.kupko11@gmail.com>
2021-10-15 13:41:39 +0300

    ...

---
+++ src/apiRouteGenerator/templates/createIndexTemplate.ts
@@ -0,0 +1,22 @@
+import { capitalizeString } from "../../utils/capitalizeString";
+
+export const createIndexTemplate = (name: string): string => {
+  const capitalizedName = capitalizeString(name);
+
+  const template = `
+	import { Router } from 'express';
+	import { create${capitalizedName} } from './post';
+	import { get${capitalizedName}s, get${capitalizedName}ById } from './get';
+
+	const router = Router();
+
+	router.post('/', create${capitalizedName});
+	router.get('/', get${capitalizedName}s);
+
+	router.get('/:id', get${capitalizedName}ById);
+
+	export default router;
+	`;
+
+  return template;
+};
---
+++ src/utils/capitalizeString.ts
@@ -0,0 +1,3 @@
+export const capitalizeString = (string: string): string => {
+  return `${string[0].toUpperCase()}${string.substring(1)}`;
+};
